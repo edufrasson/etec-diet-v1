45078e96cefc64c9e312c3081fdf7ae25b618628
Eduardo Frasson Domine <eduardo.domine@etec.sp.gov.br>
2022-03-10 20:16:57 -0300

    first commit

---
+++ View/modules/CadastrarCategoriaAlimento.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Categoria de Alimentos</title>
+    <style>
+        label, input { display: block;}
+    </style>
+</head>
+<body>
+    <form action="/save" method="post">
+        <fieldset>
+            <legend>Cadastro de Categoria de Alimentos</legend>
+            <label for="descricao">Descricao:</label>
+            <input name="descricao" id="descricao" type="text" />
+            <button type="submit">Enviar</button>
+        </fieldset>
+    </form>    
+</body>
+</html>
